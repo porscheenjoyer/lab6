61b997e1f3455897184a98dc8c9daa4e26c19942
Period 6 <braylen.diec@talawanda.org>
2024-02-27 13:28:09 -0500

    128

--- src/Task3.java
+++ src/Task3.java
@@ -14,24 +14,28 @@ public class Task3 {
             System.out.println("What is your political affiliation? Put R for Republican, D for Democratic, I for Independent, and O for Other");
 
             userParty = in.nextLine();
-            userParty.toLowerCase();
+            userParty = userParty.toLowerCase();
 
             switch (userParty) {
 
                 case "r":
                     System.out.println("You are a Republican Person");
+                    done = true;
                     break;
                 case "d":
                     System.out.println("You are a Democratic Donkey");
+                    done = true;
                     break;
                 case "i":
                     System.out.println("You are an Independent Elephant");
+                    done = true;
                     break;
                 case "o":
                     System.out.println("You are Other");
+                    done = true;
                     break;
                 default:
-                    System.out.println("You entered an incorrect value: " + userParty);
+                    System.out.println("You entered an incorrect value: " + userParty + "\nTry again");
 
             }
 
