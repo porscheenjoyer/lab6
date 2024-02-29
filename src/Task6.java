d7a427ac27c893f1c9fe7fbf7cb8cc93a0201753
Period 6 <braylen.diec@talawanda.org>
2024-02-29 13:44:23 -0500

    144

--- src/Task6.java
+++ src/Task6.java
@@ -5,6 +5,71 @@ public class Task6 {
 
         //Variables
         Scanner in = new Scanner(System.in);
+        int userNum1 = 0;
+        int userNum2 = 0;
+        boolean done = false;
+        String trash = " ";
+
+        //Loop
+        while (!done) {
+
+            System.out.println("What is your first number?");
+
+            if (in.hasNextInt()) {
+
+                userNum1 = in.nextInt();
+                done = true;
+
+            }
+
+            else {
+
+                trash = in.nextLine();
+                System.out.println("The first number must be an integer, you said: " + trash);
+
+            }
+
+        }
+
+        done = false;
+
+        while (!done) {
+
+            System.out.println("What is the second number?");
+
+            if (in.hasNextInt()) {
+
+                userNum2 = in.nextInt();
+                done = true;
+
+            }
+
+            else {
+
+                trash = in.nextLine();
+                System.out.println("The second number must be an integer, you said: " + trash);
+
+            }
+
+        }
+
+        if (userNum1 == userNum2) {
+
+            System.out.println(userNum1 + " is equal to " + userNum2);
+
+        }
+
+        else if (userNum1 > userNum2) {
+
+            System.out.println(userNum1 + " is greater than " + userNum2);
+
+        }
+
+        else {
+
+            System.out.println(userNum1 + " is less than " + userNum2);
+
+        }
 
     }
 }
